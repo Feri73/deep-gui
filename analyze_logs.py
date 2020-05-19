adds normalization other than zscore to simple analysis adds option to change summary axes in simple analysis adds option to chage the chunk start adds more log to coverage update in phone fixes bug

analyze_logs.py
@@ -12,7 +12,8 @@ from scipy import stats
 import tensorflow as tf
 
 Logs = Dict[str, np.ndarray]
-AnalysisResult = Tuple[Tuple[Union[Logs, Tuple[Logs, Logs]], str, List[List[str]]], ...]
+DimVals = List[Union[str, List[str]]]
+AnalysisResult = Tuple[Tuple[Union[Logs, Tuple[Logs, Logs]], str, DimVals], ...]
 
 
 def read_logs(logs_dir: str, tags: List[str], tools: List[str], apps: List[str], runs_per_app: int,
@@ -101,10 +102,14 @@ def read_logs(logs_dir: str, tags: List[str], tools: List[str], apps: List[str],
     return result
 
 
-def _write_values(tag: str, values: np.ndarray, errors: Optional[np.ndarray], dim_vals: List[List[str]],
+def _write_values(tag: str, values: np.ndarray, errors: Optional[np.ndarray], dim_vals: DimVals,
                   dim_val_is: List[int], writers: Dict[str, tf.summary.FileWriter],
                   logs_dir: str, analysis: str) -> None:
-    if values.ndim == 1:
+    if values.ndim == 0 or dim_vals[len(dim_val_is)] is None:
+        if values.ndim == 0:
+            values = [values]
+            if errors is not None:
+                errors = [errors]
         for i, v in enumerate(values):
             summary = tf.Summary()
             summary.value.add(tag=tag, simple_value=v)
@@ -112,7 +117,8 @@ def _write_values(tag: str, values: np.ndarray, errors: Optional[np.ndarray], di
                 summary.value.add(tag=tag, simple_value=v + errors[i])
                 summary.value.add(tag=tag, simple_value=v - errors[i])
                 summary.value.add(tag=tag, simple_value=v)
-            key = analysis + '_' + '_'.join([dim_vals[i][d_i] for i, d_i in enumerate(dim_val_is)])
+            key = analysis + '_' + '_'.join([f'{dim_vals[j]}{d_i}' if isinstance(dim_vals[j], str)
+                                             else dim_vals[j][d_i] for j, d_i in enumerate(dim_val_is)])
             if key not in writers:
                 path = f'{logs_dir}/{key}'
                 if os.path.isdir(path):
@@ -128,7 +134,9 @@ def _write_values(tag: str, values: np.ndarray, errors: Optional[np.ndarray], di
 def write_logs(logs: Logs, error_logs: Optional[Logs], dim_vals: List[List[str]], logs_dir: str, analysis: str):
     writers = {}
     for tag, values in logs.items():
-        assert values.shape[:-1] == tuple(len(x) for x in dim_vals)
+        for i, vs in enumerate(dim_vals[:-1]):
+            assert isinstance(vs, str) or values.shape[i] == len(vs)
+        assert dim_vals[-1] is None or isinstance(dim_vals[-1], str) or values.shape[-1] == len(dim_vals[-1])
         _write_values(tag, values, None if error_logs is None else error_logs[tag],
                       dim_vals, [], writers, logs_dir, analysis)
     for writer in writers.values():
@@ -181,32 +189,38 @@ def error_logs(logs: Logs, axes=List[int], **kwargs) -> Logs:
 
 def simple_analysis(logs: Logs, args: argparse.Namespace) -> AnalysisResult:
     parser = argparse.ArgumentParser('Simple Analysis')
-    parser.add_argument('--zscore', action='store_true')
-    parser.add_argument('--zscore-ref', action='store', type=str, choices=['all', *args.tools])
-    parser.add_argument('--zscore-axes', action='store', nargs='+', type=str)
+    parser.add_argument('--norm-type', action='store', type=str, choices=['mean', 'zscore'])
+    parser.add_argument('--norm-ref', action='store', type=str, choices=['all', *args.tools])
+    parser.add_argument('--norm-axes', action='store', nargs='+', type=str)
+    parser.add_argument('--summary-axes', action='store', nargs='+', type=str, required=True)
     args = parser.parse_args(args.args[1:], namespace=args)
-    assert args.zscore == (args.zscore_ref is not None) == (args.zscore_axes is not None)
-    assert args.zscore_axes is None or args.zscore_ref == 'all' or 'tool' not in args.zscore_axes
+    assert (args.norm_type is not None) == (args.norm_axes is not None)
+    assert args.norm_type is None or args.norm_ref is None or 'tool' not in args.norm_axes
 
-    if args.zscore:
-        dims = ['tool', 'app', 'run', 'time']
-        axes = [dims.index(axis) for axis in args.zscore_axes]
+    dims = ['tool', 'app', 'run', 'time']
+    dim_vals = [args.tools, args.apps, 'run', None]
 
-        if args.zscore_ref == 'all':
-            logs = zscore_logs(logs, axes=axes)
-        else:
-            ref_index = args.tools.index(args.zscore_ref)
-            ref_mean = mean_logs(logs, axes=axes, keep_shape=True)
-            ref_std = std_logs(logs, axes=axes, keep_shape=True)
-            for tag in logs.keys():
-                logs[tag] = (logs[tag] - ref_mean[tag][ref_index: ref_index + 1]) / \
-                            (ref_std[tag][ref_index: ref_index + 1] + np.finfo(float).eps)
+    if args.norm_type is not None:
+        norm_axes = sorted([dims.index(axis) for axis in args.norm_axes])
 
-    means = mean_logs(logs, axes=[1, 2])
-    errors = error_logs(logs, axes=[1, 2])
+        ref_mean = mean_logs(logs, axes=norm_axes, keep_shape=True)
+        ref_std = std_logs(logs, axes=norm_axes, keep_shape=True)
+        ref_slice = slice(None)
+        if args.norm_ref is not None:
+            ref_index = args.tools.index(args.norm_ref)
+            ref_slice = slice(ref_index, ref_index + 1)
+        for tag in logs.keys():
+            logs[tag] -= ref_mean[tag][ref_slice]
+            if args.norm_type == 'zscore':
+                logs[tag] /= ref_std[tag][ref_slice] + np.finfo(float).eps
+
+    summary_axes = sorted([dims.index(axis) for axis in args.summary_axes])
+    summary_dim_vals = [dim_val for i, dim_val in enumerate(dim_vals) if i not in summary_axes]
+    means = mean_logs(logs, axes=summary_axes)
+    errors = error_logs(logs, axes=summary_axes)
     name_prefix = 'simple'
-    return ((means, name_prefix + '_means', [args.tools]),
-            ((means, errors), name_prefix + '_errors', [args.tools]))
+    return ((means, name_prefix + '_means', summary_dim_vals),
+            ((means, errors), name_prefix + '_errors', summary_dim_vals))
 
 
 parser = argparse.ArgumentParser()

phone.py
@@ -69,15 +69,18 @@ class Phone:
         try:
             self.adb('shell am broadcast -a edu.gatech.m3.emma.COLLECT_COVERAGE')
             coverage_path = os.path.abspath(f'.cov_tmp-{self.device_name}.ec')
+            print(f'{datetime.now()}: downloading coverage for {apk_name} in {self.device_name}')
             self.adb(f'pull /mnt/sdcard/coverage.ec "{coverage_path}"')
             while not os.path.isfile(f'{coverage_path}'):
                 continue
             self.adb(f'shell rm /mnt/sdcard/coverage.ec')
             command = f'java -cp "{self.emma_jar_path}" emma report -r txt --in "{apk_name}.em" -in "{coverage_path}"' \
                       f' -Dreport.txt.out.file="{coverage_path}.txt"'
+            print(f'{datetime.now()}: running emma report for {apk_name} in {self.device_name}')
             subprocess.check_output(command, shell=True)
             cov_sum = np.zeros(4)
             all_sum = np.zeros(4)
+            print(f'{datetime.now()}: parsing emma report for {apk_name} in {self.device_name}')
             with open(f'{coverage_path}.txt', 'r') as report_file:
                 lines = report_file.readlines()
                 in_table = False
@@ -354,8 +357,8 @@ class DummyPhone:
     def is_in_app(self, app_name: str, force_front: bool) -> bool:
         return True
 
-    def update_code_coverage(self, apk_name: str, ec_file_name: str = None) -> float:
-        return 0.0
+    def update_code_coverage(self, apk_name: str, ec_file_name: str = None) -> Optional[Tuple[float, ...]]:
+        return None
 
     def screenshot(self, perform_checks: bool = False) -> np.ndarray:
         if self.screen is None:

single_state_categorical_reward_agent.py
@@ -389,6 +389,7 @@ class Coordinator(ABC, EnvironmentCallbacks):
 
         self.learner = None
         self.file_completions = defaultdict(list)
+        self.environment_completion_count = 0
 
         self.learner_thread = None
         self.collector_threads = []
@@ -418,6 +419,12 @@ class Coordinator(ABC, EnvironmentCallbacks):
     def on_episode_end(self, premature: bool) -> None:
         self.get_thread_locals().pop_and_run_next(self)
         self.local_update_collector_weight()
+
+    def on_environment_finished(self) -> None:
+        self.learner_thread.add_to_run_queue(Coordinator.record_environment_completion)
+
+    def record_environment_completion(self):
+        self.environment_completion_count += 1
 
     def local_set_new_weight(self, new_weight: List[tf.Tensor]) -> None:
         self.get_thread_locals().new_weight = new_weight
@@ -470,7 +477,7 @@ class Coordinator(ABC, EnvironmentCallbacks):
         else:
             print(f'{datetime.now()}: sending dummy to workers.')
             self.send_to_workers(Coordinator.dummy)
-        while True:
+        while self.environment_completion_count < len(self.collector_creators) + len(self.tester_creators):
             self.learner_thread.pop_and_run_next(self)
             time.sleep(1)
 
@@ -799,6 +806,7 @@ class CollectorLogger(EnvironmentCallbacks):
         self.log_preprocessed_screen = cfg['log_preprocessed_screen']
         self.log_reward_prediction = cfg['log_reward_prediction']
         self.steps_per_app = cfg['steps_per_app']
+        self.chunk_start = cfg['chunk_start']
         self.environment = None
 
         assert self.coverage_log_frequency % self.scalar_log_frequency == 0
@@ -831,6 +839,9 @@ class CollectorLogger(EnvironmentCallbacks):
                              partial(self.on_new_scalar,
                                      'Predictions/Std'), True)(tf.math.reduce_std(reward_prediction[0]))
             ]
+
+    def get_chunk(self) -> int:
+        return self.local_step // self.steps_per_new_file + self.chunk_start
 
     def set_environment(self, environment: RelevantActionEnvironment):
         self.environment = environment
@@ -865,12 +876,11 @@ class CollectorLogger(EnvironmentCallbacks):
                 self.log_scalar('Metrics/Activity Count', self.activity_count)
                 self.log_scalar('Metrics/Activity Count Percentage',
                                 np.array(self.activity_count) / len(self.environment.phone.get_app_all_activities(
-                                    self.environment.get_current_app(apk=True,  step=self.local_step - 1))))
+                                    self.environment.get_current_app(apk=True, step=self.local_step - 1))))
             if self.local_step % self.coverage_log_frequency == 0:
-                chunk = self.local_step // self.steps_per_new_file
                 coverages = self.environment.phone.update_code_coverage(
                     self.environment.get_current_app(apk=True, step=self.local_step - 1),
-                    f'{self.name}_{chunk}_{self.local_step}')
+                    f'{self.name}_{self.get_chunk()}_{self.local_step}')
                 if coverages is None:
                     coverages = [np.nan] * 4
                 self.log_scalar('Coverage/Class', coverages[0])
@@ -894,13 +904,15 @@ class CollectorLogger(EnvironmentCallbacks):
         self.preprocessed_screen = None
         self.clustering = None
 
-    def on_wait(self) -> None:
+    def write_summary(self) -> None:
         if self.summary_writer is not None:
             self.summary_writer.add_summary(self.summary, self.local_step)
-        self.summary = tf.Summary()
 
+    def on_wait(self) -> None:
+        self.write_summary()
+        self.summary = tf.Summary()
         if self.local_step % self.steps_per_new_file == 0:
-            chunk = self.local_step // self.steps_per_new_file
+            chunk = self.get_chunk()
             if self.summary_writer is not None:
                 self.summary_writer.close()
             self.summary_writer = tf.summary.FileWriter(f'{self.dir}/{self.name}_chunk_{chunk}')
@@ -913,6 +925,10 @@ class CollectorLogger(EnvironmentCallbacks):
             self.log_scalar('Coverage/Line', 0.0)
             self.summary_writer.add_summary(self.summary, self.local_step)
             self.summary = tf.Summary()
+
+    def on_environment_finished(self) -> None:
+        self.write_summary()
+        self.summary_writer.close()
 
     def on_new_preprocessed_screen(self, screen: np.ndarray) -> None:
         # assert self.preprocessed_screen is None