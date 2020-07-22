becea6f7227ceb260d3df2dfae1a3c4153bc63ae
Faraz Yazdani <ferisampad@deep-gui-performance.us-west2-a.c.encoded-copilot-259700.internal>
2020-07-22 04:08:23 -0700

    fixes bugs

--- relevant_action_monkey_client.py
+++ relevant_action_monkey_client.py
@@ -4,6 +4,7 @@ import os
 import time
 import socket
 import subprocess
+import traceback
 from typing import Any, Callable, Union, Optional
 
 import numpy as np
@@ -35,7 +36,8 @@ class RelevantActionMonkeyClient(Environment):
         self.global_equality_threshold = cfg['global_equality_threshold']
 
         self.socket = None
-        self.connected = False
+        self.control_socket = None
+        self.pinged = False
         self.current_state = None
         self.finished = True
         if not os.path.exists(f'.client_screenshots'):
@@ -48,57 +50,89 @@ class RelevantActionMonkeyClient(Environment):
             return res.decode('utf-8')
         return res
 
-    def connect_control(self):
+    def start(self):
+        while True:
+            try:
+                super().start()
+                break
+            except Exception:
+                print(f'{datetime.now()}: exception in {self.server_port}:\n{traceback.format_exc()}')
+                self.on_error()
+
+    def check_control(self) -> None:
         if self.control_port is None:
             return
         print(f'{datetime.now()}: probing control port in {self.server_port}.')
+        if self.control_socket is None:
+            self.control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.control_socket.setblocking(0)
+            self.control_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.control_socket.bind(('localhost', self.control_port))
+            self.control_socket.listen()
         try:
-            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.socket.setblocking(False)
-            self.socket.connect(('localhost', self.control_port))
-            data = self.socket.recv(512)
+            con, _ = self.control_socket.accept()
+            data = con.recv(512, socket.MSG_DONTWAIT)
             if len(data) > 0:
                 self.control_handler(data)
-        except ConnectionRefusedError:
+            con.close()
+        except BlockingIOError:
             pass
 
-    def connect(self, expected: Optional[str], blocking: bool = True, allow_control: bool = True) -> bool:
+    def connect(self, check_control:bool = False) -> None:
         while True:
-            if allow_control:
-                self.connect_control()
+            if check_control:
+                self.check_control()
             try:
                 self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                if not blocking:
-                    self.socket.setblocking(blocking)
                 self.socket.connect(('localhost', self.server_port))
-                data = self.socket.recv(512)
-                if len(data) > 0:
-                    if expected is None or data == f'OK:{expected}\n'.encode():
-                        print(f'{datetime.now()}: received {data} from {self.server_port}')
-                        self.connected = True
-                        return True
-                    raise NotImplementedError(f'expected {expected} got {data}')
-                elif not blocking:
-                    return False
+                print(f'{datetime.now()}: conntected to server {self.server_port}.')
+                return
             except ConnectionRefusedError:
-                if not blocking:
-                    return False
-                pass
-            print(f'{datetime.now()}: server {self.server_port} is down. trying again in 0.5 seconds.')
-            time.sleep(0.5)
-            continue
+                print(f'{datetime.now()}: server {self.server_port} is down. trying again in 0.5 seconds.')
+                time.sleep(0.5)
+
+    def receive(self, expected: Optional[str], blocking: bool = True) -> Optional[bytes]:
+        try:
+            if blocking:
+                data = self.socket.recv(512)
+            else:
+                data = self.socket.recv(512, socket.MSG_DONTWAIT)
+            if len(data) > 0:
+                if expected is None or data == f'OK:{expected}\n'.encode():
+                    print(f'{datetime.now()}: received {data} from {self.server_port}')
+                    return data
+                raise RuntimeError(f'expected {expected} got {data}')
+            else:
+                raise ConnectionRefusedError()
+        except BlockingIOError:
+            assert not blocking
+            return None
+
+    def send(self, data:str) -> None:
+        self.socket.send((data+'\n').encode())
 
     def disconnect(self) -> None:
+        self.send('done')
         self.socket.close()
-        self.connected = False
 
     def restart(self) -> None:
-        pass
+        self.current_state = None
+        self.finished = True
 
     def read_state(self) -> np.ndarray:
-        if not self.connected:
-            self.connect('ping')
         if self.current_state is None:
+            if self.pinged:
+                self.connect()
+            else:
+                self.connect(check_control=True)
+                while True:
+                    try:
+                        self.receive('ping')
+                        self.pinged = True
+                        break
+                    except ConnectionRefusedError:
+                        time.sleep(0.5)
+                        self.connect(check_control=True)
             self.current_state = self.screenshot()
         return self.current_state
 
@@ -122,57 +156,65 @@ class RelevantActionMonkeyClient(Environment):
     def are_states_equal(self, s1: np.ndarray, s2: np.ndarray) -> bool:
         return np.linalg.norm(self.crop_state(s1) - self.crop_state(s2)) <= self.global_equality_threshold
 
+    # I assume when in this function, the connection with server is live
     def act(self, action: Any, wait_action: Callable) -> float:
+        self.pinged = False
         type = action[2]
         action = self.action2pos(action)
         if type == 0:
             print(f'{datetime.now()}: sending click on {action[0]}, {action[1]} to {self.server_port}')
             # read the output of monkey here: should be OK
-            self.socket.send(f'touch down {action[0]} {action[1]}\n'.encode())
-            self.socket.send(f'touch up {action[0]} {action[1]}\n'.encode())
+            self.send(f'touch down {action[0]} {action[1]}')
+            self.send(f'touch up {action[0]} {action[1]}')
         elif type == 1:
             up_scroll = random.uniform(0, 1) > .5
             val = random.randint(self.scroll_min_value, self.scroll_max_value) * (-1) ** up_scroll
             x, y = action[0], action[1]
-            print(f'{datetime.now()}: sending scroll {"up" if up_scroll else "down"} on {x},{y}'
+            print(f'{datetime.now()}: sending scroll {"up" if up_scroll else "down"} on {x},{y} '
                   f'to {self.server_port}')
-            self.socket.send(f'touch down {x} {y}\n'.encode())
+            self.send(f'touch down {x} {y}')
             for _y in range(y + val // self.scroll_event_count, y + val, val // self.scroll_event_count):
-                self.socket.send(f'touch move {x} {int(_y)}\n'.encode())
-            self.socket.send(f'touch up {x} {int(_y)}\n'.encode())
+                self.send(f'touch move {x} {int(_y)}')
+            self.send(f'touch up {x} {int(_y)}')
         elif type == 2:
             left_scroll = random.uniform(0, 1) > .5
             val = random.randint(self.scroll_min_value, self.scroll_max_value) * (-1) ** left_scroll
             x, y = action[0], action[1]
-            print(f'{datetime.now()}: sending swipe {"left" if left_scroll else "right"} on {x},{y}'
+            print(f'{datetime.now()}: sending swipe {"left" if left_scroll else "right"} on {x},{y} '
                   f'to {self.server_port}')
-            self.socket.send(f'touch down {x} {y}\n'.encode())
+            self.send(f'touch down {x} {y}')
             for _x in range(x + val // self.scroll_event_count, x + val, val // self.scroll_event_count):
-                self.socket.send(f'touch move {int(_x)} {y}\n'.encode())
-            self.socket.send(f'touch up {int(_x)} {y}\n'.encode())
+                self.send(f'touch move {int(_x)} {y}')
+            self.send(f'touch up {int(_x)} {y}')
         else:
             raise NotImplementedError()
-        self.socket.send(f'done\n'.encode())
         self.disconnect()
+        self.pinged = False
 
+        self.connect()
         reward = 0
-        if type == 0:
-            while not self.connect('action_done', blocking=False, allow_control=False):
-                self.disconnect()
-                shot = self.screenshot()
-                if not self.are_states_equal(shot, self.current_state):
-                    reward = 1
-                    break
-                time.sleep(self.screenshots_interval)
-        else:
-            self.connect('action_done', allow_control=False)
+        try:
+            if type == 0:
+                while not self.receive('action_done', blocking=False):
+                    shot = self.screenshot()
+                    if not self.are_states_equal(shot, self.current_state):
+                        reward = 1
+                        self.receive('action_done')
+                        break
+                    time.sleep(self.screenshots_interval)
+            else:
+                self.receive('action_done')
+        except RuntimeError:
+            # I assume it is a ping
+            self.pinged = True
+            raise
         shot = self.screenshot()
         if not self.are_states_equal(shot, self.current_state):
             reward = 1
-        self.socket.send(f'done\n'.encode())
         self.disconnect()
 
-        self.current_state = None
+        self.current_state = shot
         wait_action()
 
         return reward * self.pos_reward + (1 - reward) * self.neg_reward
+
